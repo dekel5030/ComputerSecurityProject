3d3cd16a3471997b950e853483ced80d486e84c6
Zohar <zoko@yahoo.com>
2024-11-28 00:57:34 +0200

    basic message

--- ComputerSecurityProject/settings.py
+++ ComputerSecurityProject/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or another email service
+EMAIL_HOST = 'smtp.ethereal.email'  # or another email service
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Computesecurityproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'Computersecurity'
-DEFAULT_FROM_EMAIL = 'noreply@gmail.com'
+EMAIL_HOST_USER = 'devonte54@ethereal.email'
+EMAIL_HOST_PASSWORD = 'eyTezvcsvrmQMUebfB'
+DEFAULT_FROM_EMAIL = 'devonte54@ethereal.email'
--- accounts/send_email.py
+++ accounts/send_email.py
@@ -5,7 +5,7 @@ def send_email_with_parameter(request, parameter_value):
     subject = "Hello, this is a test email!"
     message = f"Your parameter value is: {parameter_value}"
     from_email = settings.DEFAULT_FROM_EMAIL  # You can set this in your settings.py
-    recipient_list = ['recipient@example.com']  # Replace with actual recipient's email
+    recipient_list = ['zoko177@yahoo.com']  # Replace with actual recipient's email
 
     send_mail(subject, message, from_email, recipient_list)
 
