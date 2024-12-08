80062ea1091fc6f0aaadb4645730f1e3040a8d23
Dekel5030 <dekel5030@gmail.com>
2024-12-08 09:50:38 +0200

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -131,4 +131,6 @@ def generate_verification_code():
     return 123456
 
 def change_password(request):
-    if request.method == "GET":return render(request, "change_password.html",{})
+    if request.method == "GET":
+        print("in")
+    return render(request, "change_password.html")
